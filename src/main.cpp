201327e5423efd6b2ea1c9369c7d744127be1288
rossalai <rossalai@msu.edu>
2017-02-16 16:46:30 -0500

    started code

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
-/* Project: 905 Project 1
- * File:   main_general.cpp
+/* Project: 905 Project 2
+ * File:   main.cpp
  * Author: alaina ross
  *
- * Created on January 12, 2017, 4:16 PM
+ * Created on February 9, 2017, 4:52 PM
  */
 
 #include <iostream>
 #include<fstream>
-#include <armadillo>
+//#include "../armadillo"
+#include </opt/local/include/armadillo.h>
 #include<time.h>
 using namespace std;
 using namespace arma;
@@ -21,107 +22,89 @@ int main(int argc, char** argv) {
     int n;
     cout<<"n: ";
     cin >> n;
+    double pmin=0;
+    double pmax=10;      
 
-    double h = 1/(double(n)+1);
+    double h = (pmax-pmin)/(double(n));
     clock_t begin,finish;
     
     mat a = zeros<mat>(n,n);
-    vec b(n);
+    vec b = zeros<vec>(n);
     vec v = zeros<vec>(n);
-    vec x(n);
+    vec r(n);
     
     //initialize x values
-    x(0)=h;
-    for (int i=1; i<n ;i++)
-        x(i)=x(i-1)+h;
+    r(0)=0;
+    for (int i=1; i<n ;i++){
+        r(i)=r(i-1)+h;
+    }
 
     
     //initialize matrix and vector
     for (int i=0;i<n;i++){
         for (int j=0;j<n;j++){
             if(i==j)
-                a(i,j)=2;
-            else if (i==j+1 or i==j-1)
-                a(i,j)=-1;
+                a(i,j)=(2/(h*h))+r(i)*r(i);
+            else if (i==j+1 or i==j-1){
+                a(i,j)=-1/(h*h);
+                a(i,j)=1+j-i*i;
+            }
+            
         }
-        b(i)=h*h*100*exp(-10*(x(i)));
+        //b(i)=h*h*100*exp(-10*(x(i)));
     }
     
     if(n<=10){
-        cout<<"Before Gauss elimination"<<endl;
+        //cout<<"Before LU decomp"<<endl;
         print_vals(a,b,v,n);
     }
     
+    double max=0;
+    int p=0;
+    int q=0;
     begin=clock();
     
-      //LU decomposition    
-//    mat L=trimatl(a);
-//    mat U=trimatu(a);
-//    vec y=solve(trimatl(L),b);
-//    v=solve(trimatu(U),y);
-    
-    
-    //do forward elimination
-    double temp=0;
-    for (int k=0;k<=n-2;k++){
-        for (int i=k+1;i<=n-1;i++){
-            temp=a(i,k)/a(k,k);
-            for ( int j=k;j<=n-1;j++){
-                a(i,j)-=a(k,j)*temp;
+    //find maximum non-diag matrix elements
+    for (int i=0;i<n;i++){
+         for (int j=0;j<n;j++){
+            if(i!=j && abs(a(i,j))>max){
+                max=a(i,j);
+                p=i;
+                q=j;
             }
-            b(i)-=temp*b(k);
-            if(abs(b(i))<=pow(10,-14))
-                b(i)=0;
-        }
+         }
     }
-
-    //do back substitution
-    if(abs(a(n-1,n-1))<=pow(10,-14)){
-        v(n-1)=1;
-        a(n-1,n-1)=0;
-    }
-    else{
-        v(n-1)=b(n-1)/a(n-1,n-1);
-    }
-    double sum=0;
-    for (int i=n-2;i>=0;i--){
-        for (int j=n-1;j>i;j--){
-            sum+=a(i,j)*v(j);
-        }
-        v(i)=(b(i)-sum)/a(i,i);
-        if(abs(v(i))<=pow(10,-14))
-            v(i)=0;
-        sum=0;
-    }
+  
+//    Unit test for max(a(i,j))    
+    cout<<endl<<"Testing max non-diagonal matrix element"<<endl;
+    cout<<"Results should be: max = -6 p,q = 3,2"<<endl;
+    cout<<"max: "<<max<<endl;
+    cout<<"p,q: "<<p<<","<<q<<endl<<endl;
+    
     
     finish=clock();
-    
-    if(n<=10){
-        cout<<"After Gauss elimination"<<endl;
-        print_vals(a,b,v,n); 
-    }
     
     cout<<"Total CPU time (sec) : "<<((double)finish-(double)begin)/
             CLOCKS_PER_SEC<<endl;
     
     
-    vec u(n);
-    ofstream outfile("output_general.txt");
-    ofstream error("error_general.txt");
-    ofstream actual("output_analytic.txt");
-    outfile<<"0.0 0.0"<<endl;
-    actual<<"0.0 0.0"<<endl;
-    for (int i=0;i<n;i++){
-        outfile<<x(i)<<" "<<v(i)<<endl;
-        u(i)=1-(1-exp(-10))*x(i)-exp(-10*x(i));
-        actual<<x(i)<<" "<<u(i)<<endl;
-        error<<x(i)<<" "<<log10(abs((v(i)-u(i))/u(i)))<<endl;
-    }
-    outfile<<"1.0 0.0"<<endl;
-    actual<<"1.0 0.0"<<endl;
-    outfile.close();
-    actual.close();
-    error.close();
+//    vec u(n);
+//    ofstream outfile("output_general.txt");
+//    ofstream error("error_general.txt");
+//    ofstream actual("output_analytic.txt");
+//    outfile<<"0.0 0.0"<<endl;
+//    actual<<"0.0 0.0"<<endl;
+//    for (int i=0;i<n;i++){
+//        outfile<<x(i)<<" "<<v(i)<<endl;
+//        u(i)=1-(1-exp(-10))*x(i)-exp(-10*x(i));
+//        actual<<x(i)<<" "<<u(i)<<endl;
+//        error<<x(i)<<" "<<log10(abs((v(i)-u(i))/u(i)))<<endl;
+//    }
+//    outfile<<"1.0 0.0"<<endl;
+//    actual<<"1.0 0.0"<<endl;
+//    outfile.close();
+//    actual.close();
+//    error.close();
     
     
     return 0;
