f7668e849e6ac0ad8a393bff4d47d19c912932b0
rossalai <rossalai@msu.edu>
2017-02-28 15:01:17 -0500

    finished unit tests

--- tests/test-functions.cpp
+++ tests/test-functions.cpp
@@ -6,35 +6,68 @@ TEST_CASE("Testing max a(i,j)"){
     int n=3;
     double pmin=0, pmax=10,h = (pmax-pmin)/(double(n));
     mat a = zeros<mat>(n,n);
+    mat v = zeros<mat>(n,n);
     vec r(n);
-    r(0)=h;
-    for (int i=1; i<n ;i++){
-        r(i)=r(i-1)+h;
-    }
-    //initialize matrix and vector
-    for (int i=0;i<n;i++){
-        for (int j=0;j<n;j++){
-            if(i==j){
-                a(i,j)=2/(h*h)+r(i)*r(i);
-            }
-            else if (i==j+1 or i==j-1){
-                a(i,j)=-1/(h*h);
-            } 
-        }
-    }
+    //initialize matrices and vector
+    initialize(n,h,a,r,v,0,0);
     int p=0;
     int q=0;
     double apq=0;
+    //find maximum matrix element
     find_max(a,p,q,apq,n);
+    
     REQUIRE(p==2);
     REQUIRE(q==1);
     REQUIRE(apq==Approx(-0.09));
-    
 }
 
 TEST_CASE("Testing eigenvalues"){
+    int n=3,interact=0;
+    double conv=0.001,wr=0.01, pmin=0, pmax=10,h = (pmax-pmin)/(double(n));
+    mat a = zeros<mat>(n,n);
+    mat v = zeros<mat>(n,n);
+    vec r(n);
+    //initialize matrices and vector
+    initialize(n,h,a,r,v,0,0);
+    //do jacobi algorithm until convergence
+    jacobi(n,interact,conv,wr,a,r,v);
+    //get eigenvalue vector
+    vector<double>eigen=get_eigenvals(a,n);
     
+    REQUIRE(eigen[0]==Approx(11.2909));
+    REQUIRE(eigen[1]==Approx(44.6241));
+    REQUIRE(eigen[2]==Approx(100.18));
 }
 TEST_CASE("Testing eigenvector orthogonality"){
+    int n=3,interact=0;
+    double conv=0.001,wr=0.01, pmin=0, pmax=10,h = (pmax-pmin)/(double(n));
+    mat a = zeros<mat>(n,n);
+    mat v = zeros<mat>(n,n);
+    vec r(n);
+    //initialize matrices and vector
+    initialize(n,h,a,r,v,0,0);
+    //do jacobi algorithm until convergence
+    jacobi(n,interact,conv,wr,a,r,v);
+    mat eigenvec=get_eigenvecs(a,v,n);
+
+    //test eigen vectors
+    REQUIRE(eigenvec(0,0)==Approx(0.9999).epsilon(0.001));
+    REQUIRE(eigenvec(0,1)==Approx(-0.0027));
+    REQUIRE(eigenvec(0,2)==Approx(0.0000));
+    REQUIRE(eigenvec(1,0)==Approx(-0.0027));
+    REQUIRE(eigenvec(1,1)==Approx(0.9999).epsilon(0.001));
+    REQUIRE(eigenvec(1,2)==Approx(-0.0017).epsilon(0.001));
+    REQUIRE(eigenvec(2,0)==Approx(0.0000));
+    REQUIRE(eigenvec(2,1)==Approx(-0.0017).epsilon(0.001));
+    REQUIRE(eigenvec(2,2)==Approx(0.9999).epsilon(0.001));
     
+    //test eigen vector orthogonality
+    //dot1=v0*v1=0
+    double dot1=eigenvec(0,0)*eigenvec(1,0)+eigenvec(0,1)*eigenvec(1,1)
+        +eigenvec(0,2)*eigenvec(1,2);
+    //dot2=v0*v0=1
+    double dot2=eigenvec(0,0)*eigenvec(0,0)+eigenvec(0,1)*eigenvec(0,1)
+        +eigenvec(0,2)*eigenvec(0,2);
+    REQUIRE(dot1==Approx(0.000).epsilon(0.01));
+    REQUIRE(dot2==Approx(1.000));
 }
