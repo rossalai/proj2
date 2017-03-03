807b4a74b0ee1c4b6071ca96d8e3897c1f1b2f30
rossalai <rossalai@msu.edu>
2017-03-03 15:02:50 -0500

    updated tests

--- tests/test-functions.cpp
+++ tests/test-functions.cpp
@@ -1,5 +1,5 @@
 #include "catch.hpp"
-#include "proj2.h"
+#include "jacobi.h"
 
 
 TEST_CASE("Testing max a(i,j)"){
@@ -22,8 +22,9 @@ TEST_CASE("Testing max a(i,j)"){
 }
 
 TEST_CASE("Testing eigenvalues"){
-    int n=3,interact=0;
+    int n=4,interact=0;
     double conv=0.001,wr=0.01, pmin=0, pmax=10,h = (pmax-pmin)/(double(n));
+    n=n-1;
     mat a = zeros<mat>(n,n);
     mat v = zeros<mat>(n,n);
     vec r(n);
@@ -34,13 +35,14 @@ TEST_CASE("Testing eigenvalues"){
     //get eigenvalue vector
     vector<double>eigen=get_eigenvals(a,n);
     
-    REQUIRE(eigen[0]==Approx(11.2909));
-    REQUIRE(eigen[1]==Approx(44.6241));
-    REQUIRE(eigen[2]==Approx(100.18));
+    REQUIRE(eigen[0]==Approx(6.56863));
+    REQUIRE(eigen[1]==Approx(25.32055));
+    REQUIRE(eigen[2]==Approx(56.57082));
 }
 TEST_CASE("Testing eigenvector orthogonality"){
-    int n=3,interact=0;
+    int n=4,interact=0;
     double conv=0.001,wr=0.01, pmin=0, pmax=10,h = (pmax-pmin)/(double(n));
+    n=n-1;
     mat a = zeros<mat>(n,n);
     mat v = zeros<mat>(n,n);
     vec r(n);
@@ -51,14 +53,14 @@ TEST_CASE("Testing eigenvector orthogonality"){
     mat eigenvec=get_eigenvecs(a,v,n);
 
     //test eigen vectors
-    REQUIRE(eigenvec(0,0)==Approx(0.9999).epsilon(0.001));
-    REQUIRE(eigenvec(0,1)==Approx(-0.0027));
-    REQUIRE(eigenvec(0,2)==Approx(0.0000));
-    REQUIRE(eigenvec(1,0)==Approx(-0.0027));
-    REQUIRE(eigenvec(1,1)==Approx(0.9999).epsilon(0.001));
-    REQUIRE(eigenvec(1,2)==Approx(-0.0017).epsilon(0.001));
-    REQUIRE(eigenvec(2,0)==Approx(0.0000));
-    REQUIRE(eigenvec(2,1)==Approx(-0.0017).epsilon(0.001));
+    REQUIRE(eigenvec(0,0)==Approx(0.9996).epsilon(0.001));
+    REQUIRE(eigenvec(0,1)==Approx(0.00853));
+    REQUIRE(eigenvec(0,2)==Approx(0.0000).epsilon(0.001));
+    REQUIRE(eigenvec(1,0)==Approx(-0.00853));
+    REQUIRE(eigenvec(1,1)==Approx(0.99995).epsilon(0.001));
+    REQUIRE(eigenvec(1,2)==Approx(0.00512).epsilon(0.001));
+    REQUIRE(eigenvec(2,0)==Approx(0.0000).epsilon(0.001));
+    REQUIRE(eigenvec(2,1)==Approx(-0.00512).epsilon(0.001));
     REQUIRE(eigenvec(2,2)==Approx(0.9999).epsilon(0.001));
     
     //test eigen vector orthogonality
